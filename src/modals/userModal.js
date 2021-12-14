94682d4168eb5930ac3457d6503a76d38e70d6df
gowtham <kasigowtham7@gmail.com>
2021-12-14 17:14:09 +0530

    user schema   initial commit

---
+++ src/modals/userModal.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+      select: false,
+    },
+    mobileno: {
+      type: Number,
+      required: true,
+    },
+    countryCode: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true, // adds createdAt and updatedAt fields in database
+  }
+);
+
+userSchema.pre("save", async function (next) {
+  // Hashing the password
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
