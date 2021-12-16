2f7d7877909607485c8224da3a4e51fc83dd6293
gowtham <kasigowtham7@gmail.com>
2021-12-16 10:12:56 +0530

    fixed bug in duplicate validation

--- src/middleware/userValidation.js
+++ src/middleware/userValidation.js
@@ -1,17 +1,18 @@
-import { check, validationResult } from "express-validator";
-import errorResponse from "../helpers/errorResponse";
+import { check, validationResult } from 'express-validator';
+import { handleError } from '../helpers/responseHandler';
+import User from '../modals/userModal';
 
 module.exports = {
   userValidationRules: () => {
     return [
-      check("email")
+      check('email')
         .isEmail()
         .normalizeEmail()
-        .withMessage("email is not valid"),
-      check("password")
+        .withMessage('email is not valid'),
+      check('password')
         .isLength({ min: 6 })
-        .withMessage("password must contain min 6 char"),
-      check("mobileno").isMobilePhone().withMessage("mobileno is not valid"),
+        .withMessage('password must contain min 6 char'),
+      check('mobileno').isMobilePhone().withMessage('mobileno is not valid'),
     ];
   },
 
@@ -20,6 +21,12 @@ module.exports = {
     if (errors.isEmpty()) return next();
     const extractedErrors = [];
     errors.array().map((err) => extractedErrors.push({ [err.param]: err.msg }));
-    return errorResponse(422, extractedErrors, res);
+    return handleError(422, extractedErrors, res);
+  },
+
+  duplicateValidation: async (req, res, next) => {
+    const duplicate = await User.find({ email: req.body.email });
+    if (duplicate.length === 0) return next();
+    return handleError(400, 'user already exists', res);
   },
 };
