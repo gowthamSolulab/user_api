3b3e929305dc5c876dcc8d2aee10a72420843c9c
gowtham <kasigowtham7@gmail.com>
2022-01-06 17:50:55 +0530

    export routes added

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 
 import user from './user/userRoutes';
 import uploadData from './uploadData/uploadDataRoutes';
+import exportData from './export/exportDataRoutes';
 
 const router = express.Router();
 
 router.use('/users', user);
 router.use('/upload-data', uploadData);
+router.use('/export-data', exportData);
 
 export default router;
